cdf0bed8671651d94f81bf936e6cd99fcfb7431b
guanshuaijie <1845543324@qq.com>
2021-02-07 15:48:53 +0800

    滚动到底部时，不再展示最新提示

--- src/views/src/components/chat-record.vue
+++ src/views/src/components/chat-record.vue
@@ -1,48 +1,85 @@
 <template>
-  <div class="room-cont" ref="room-cont">
-    <p v-for="({ data, msg, type }, i) in chatData" :key="i">
-      <span v-if="type === 'join' || type === 'leave'">
-        {{ data }} {{ msg }}
-      </span>
-      <span v-else>
-        {{ data }}: {{ msg }}
-      </span>
-    </p>
+  <div class="room-container">
+    <div class="room-cont" ref="room-cont">
+      <p v-for="({ data, msg, type }, i) in chatData" :key="i">
+        <span v-if="type === 'join' || type === 'leave'">
+          {{ data }} {{ msg }}
+        </span>
+        <span v-else>
+          {{ data }}: {{ msg }}
+        </span>
+      </p>
+    </div>
+    <span v-if="isShowNewsTip" @click="toNews" class="to-news">查看最新消息</span>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
+  setup() {
+    const isShowNewsTip = ref(false);
+    onMounted(() => {
+      // window.addEventListener('scroll');
+      console.log(isShowNewsTip.value, 'this');
+    })
+    return {
+      isShowNewsTip
+    }
+  },
   props: {
     chatData: Array
   },
   watch: {
     'chatData.length'() {
+      const { el, scrollHeight, clientHeight, scrollTop } = this.getElInfo();
+      const isBottom = scrollTop === scrollHeight - clientHeight;
+      
+      if (isBottom) {
+        setTimeout(() => { 
+          this.isShowNewsTip = false;
+          const newsTop = el.scrollHeight - el.clientHeight;
+          el.scrollTo(0, newsTop) 
+        });
+        return;
+      }
+      this.isShowNewsTip = true;
+    }
+  },
+  methods: {
+    getElInfo() {
       const el = this.$refs['room-cont'];
       const scrollHeight = el.scrollHeight,
       clientHeight = el.clientHeight,
       scrollTop = el.scrollTop;
-      const isBottom = scrollTop === scrollHeight - clientHeight;
-      
-      if (isBottom) setTimeout(() => { 
-        const newTop = el.scrollHeight - el.clientHeight;
-        el.scrollTo(0, newTop) 
-      });
-    }
-  },
-  setup() {
-    return {
+      return { el, scrollHeight, clientHeight, scrollTop };
+    },
+    toNews() {
+      const { el, scrollHeight, clientHeight, scrollTop } = this.getElInfo();
+      const newsTop = scrollHeight - clientHeight;
+      el.scrollTo(0, newsTop) 
+      this.isShowNewsTip = false;
     }
   }
 });
 </script>
 <style lang="less" scoped>
-.room-cont {
-  padding: 20px;
+.room-container {
+  position: relative;
   flex: 1;
-  overflow: auto;
-  font-size: 18px;
+  .room-cont {
+    height: 273px;
+    padding: 20px;
+    overflow: auto;
+    font-size: 18px;
+  }
+
+  .to-news {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    font-size: 14px;
+  }
 }
 </style>
