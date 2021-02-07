5d7b98a9814cc8896e33a297daf1242abad96bdc
guanshuaijie <1845543324@qq.com>
2021-02-07 16:08:00 +0800

    使用vue3 api 重构

--- src/views/src/components/chat-record.vue
+++ src/views/src/components/chat-record.vue
@@ -15,75 +15,78 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onBeforeUnmount } from 'vue';
+import { defineComponent, ref, onMounted, onBeforeUnmount, watchEffect, watch, onUnmounted } from 'vue';
 
 export default defineComponent({
-  setup() {
+  props: {
+    chatData: Array
+  },
+  
+  setup(props) {
     const isShowNewsTip = ref(false);
 
     let myRef = null;
     const setRef = e => {
       myRef = e;
     }
+    // 获取聊天内容的 DOM 数据
     const getElInfo = () => {
       const el = myRef;
+      if (!el) return {};
       const scrollHeight = el.scrollHeight,
       clientHeight = el.clientHeight,
       scrollTop = el.scrollTop;
       return { el, scrollHeight, clientHeight, scrollTop };
     }
 
+    // 滚动时监听是否查看了最新消息
     const onScroll = () => {
       const { el, scrollHeight, clientHeight, scrollTop } = getElInfo();
       const isBottom = scrollTop === scrollHeight - clientHeight;
       if (isBottom) isShowNewsTip.value = false;
     }
+
+    // 挂载
     onMounted(() => {
       const { el } = getElInfo();
       el.addEventListener('scroll', onScroll);
     })
-    onBeforeUnmount(() => {
+    // 卸载
+    onUnmounted(() => {
       const { el } = getElInfo();
       el.removeEventListener('scroll', onScroll);
     })
-    return {
-      isShowNewsTip,
-      setRef,
-      getElInfo
-    }
-  },
-  props: {
-    chatData: Array
-  },
-  watch: {
-    'chatData.length'() {
-      const { el, scrollHeight, clientHeight, scrollTop } = this.getElInfo();
+
+    // 监听聊天内容变化
+    const watchChatChange = function () {
+      const { el, scrollHeight, clientHeight, scrollTop } = getElInfo();
+      if (!el) return;
       const isBottom = scrollTop === scrollHeight - clientHeight;
       
       if (isBottom) {
         setTimeout(() => { 
-          this.isShowNewsTip = false;
+          isShowNewsTip.value = false;
           const newsTop = el.scrollHeight - el.clientHeight;
           el.scrollTo(0, newsTop) 
         });
         return;
       }
-      this.isShowNewsTip = true;
+      isShowNewsTip.value = true;
     }
-  },
-  methods: {
-    // getElInfo() {
-    //   const el = this.$refs['room-cont'];
-    //   const scrollHeight = el.scrollHeight,
-    //   clientHeight = el.clientHeight,
-    //   scrollTop = el.scrollTop;
-    //   return { el, scrollHeight, clientHeight, scrollTop };
-    // },
-    toNews() {
-      const { el, scrollHeight, clientHeight, scrollTop } = this.getElInfo();
+    watch(() => props.chatData.length, watchChatChange)
+
+    const toNews = () => {
+      const { el, scrollHeight, clientHeight, scrollTop } = getElInfo();
       const newsTop = scrollHeight - clientHeight;
       el.scrollTo(0, newsTop) 
-      this.isShowNewsTip = false;
+      isShowNewsTip.value = false;
+    }
+    
+    return {
+      isShowNewsTip,
+      setRef,
+      getElInfo,
+      toNews
     }
   }
 });
